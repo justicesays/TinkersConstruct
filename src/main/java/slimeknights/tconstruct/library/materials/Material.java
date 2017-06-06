de764aec76f6a0f6243898b69155f797050d8b27
justicesays <github@redmore.net>
2017-06-06 15:46:20 +0100

    Remove Ore Doubling (as lacking config option)

--- src/main/java/slimeknights/tconstruct/library/materials/Material.java
+++ src/main/java/slimeknights/tconstruct/library/materials/Material.java
@@ -42,7 +42,7 @@ public class Material extends RecipeMatchRegistry {
   public static final int VALUE_Fragment = VALUE_Ingot/4;
   public static final int VALUE_Shard = VALUE_Ingot/2;
 
-  public static final int VALUE_Ore = VALUE_Ingot*2;
+  public static final int VALUE_Ore = VALUE_Ingot;
   public static final int VALUE_Gem = 666; // divisible by 3!
   public static final int VALUE_Block = VALUE_Ingot*9;
 
